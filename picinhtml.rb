b701e8a2e5ac85047007ca59b1f1241529aed4aa
avapoet <dan@scatmania.org>
2010-05-24 15:07:26 +0100

    Added automatic posterisation. Optimised CSS class names.

--- picinhtml.rb
+++ picinhtml.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
-unless $*.length == 2
-  puts "Syntax: ./picinhtml.rb image-file text-file" and exit
+unless $*.length == 3
+  puts "Syntax: ./picinhtml.rb image-file text-file posterization-level" and exit
 end
 
-%w{rubygems RMagick sha1}.each do |gem|
+%w{rubygems RMagick}.each do |gem|
   begin
     require gem
   rescue LoadError => e
@@ -15,22 +15,28 @@ include Magick
 text = File::read($*[1]).gsub(/[\n\t]/, ' ').gsub('  ', ' ').strip
 text_pos = -1
 
-salt = SHA1::hexdigest("#{Time::now.to_s}#{text}")
-
 colors, content = {}, ''
-image = Image::read($*[0]).first
+image = Image::read($*[0]).first.posterize($*[2].to_i)
 image.rows.times do |y|
   content << "<div class=\"line\">"
   image.columns.times do |x|
     px = image.pixel_color(x, y)
     red, green, blue = (px.red / 256).floor, (px.green / 256).floor, (px.blue / 256).floor
-    hash = SHA1::hexdigest("#{salt}-#{red}-#{green}-#{blue}")[0,16]
-    colors[hash] = "color: rgb(#{red}, #{green}, #{blue}); background: rgb(#{red}, #{green}, #{blue});"
-    content << "<span class=\"c#{hash}\">#{text[text_pos += 1, 1]}</span>"
+    css = "rgb(#{red},#{green},#{blue})"
+    unless id = colors[css]
+      id = colors.length
+      colors[css] = id
+    end
+    content << "<span class=\"c#{id}\">#{text[text_pos += 1, 1]}</span>"
   end
   content << "</div>"
 end
-style = ".picinhtml { white-space: pre; font-family: monospace; font-size: 10px; line-height: 10px; }"
-style << colors.collect { |key, value| ".picinhtml .c#{key}::selection { #{value} }\n.picinhtml .c#{key}::-moz-selection { #{value} }" }.join("\n")
+style = ".picinhtml { white-space: pre; font-family: monospace; font-size: 8px; line-height: 8px; letter-spacing: 2px; }\n"
+colors = colors.invert
+colors.length.times do |id|
+  css = colors[id]
+  style << ".picinhtml .c#{id}::selection {color:#{css};background:#{css}}\n"
+  style << ".picinhtml .c#{id}::-moz-selection {color:#{css};background:#{css}}\n"
+end
 
-puts "<!DOCTYPE html>\n<html><head>\n<title>#{$*[0]}</title>\n<style type=\"text/css\" media=\"screen\">\n#{style}\n</style>\n</head><body><div class=\"picinhtml\">#{content}</div></body></html>"
+puts "<!DOCTYPE html>\n<html><head>\n<title>#{$*[0].gsub(/\..*?$/, '')}</title>\n<style type=\"text/css\" media=\"screen\">\n#{style}</style>\n</head><body><div class=\"picinhtml\">#{content}</div></body></html>"
